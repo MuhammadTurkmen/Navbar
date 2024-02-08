301bd693af4d1b2af9c08ab5856e6dfd3e691aaf
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-08 13:32:00 +0430

    init

--- app.js
+++ app.js
@@ -8,10 +8,5 @@ const navToggle = document.querySelector('.nav-toggle')
 const links = document.querySelector('.links')
 
 navToggle.addEventListener('click', function() {
-    if(links.classList.contains('show-links')) {    
-        links.classList.remove('show-links')
-    }
-    else {
-        links.classList.add('show-links')
-    }
+    
 })
